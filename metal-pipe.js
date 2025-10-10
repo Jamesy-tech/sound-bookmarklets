f09fa7eb260af64f12341bef189ad4164e600e36
jamesy-tech <jamesycreator@gmail.com>
2025-10-10 16:55:02 +1300

    Update metal-pipe.js

--- metal-pipe.js
+++ metal-pipe.js
@@ -1,4 +1,4 @@
 javascript:(function(){
-  var audio = new Audio('https://github.com/Jamesy-tech/sound-bookmarklets/raw/main/pipe-clang.mp3');
+  var audio = new Audio('https://github.com/Jamesy-tech/sound-bookmarklets/raw/main/metal-pipe.mp3');
   audio.play().catch(function(e){console.log('Audio play failed:', e)});
 })();
