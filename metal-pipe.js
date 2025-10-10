ace9a88fcc9700358886069535460702d524ad87
jamesy-tech <jamesycreator@gmail.com>
2025-10-10 16:49:01 +1300

    Update metal-pipe.js

--- metal-pipe.js
+++ metal-pipe.js
@@ -1,4 +1,4 @@
 javascript:(function(){
-  var audio = new Audio('https://www.myinstants.com/media/sounds/jixaw-metal-pipe-falling-sound.mp3');
+  var audio = new Audio('https://github.com/Jamesy-tech/sound-bookmarklets/raw/main/pipe-clang.mp3');
   audio.play().catch(function(e){console.log('Audio play failed:', e)});
 })();
