ccfae1e1a33ec6eeb16841ce254ae287c48f5a0b
jamesy-tech <jamesycreator@gmail.com>
2026-03-21 09:26:51 +1300

    Create roblox-news-guy.js

---
+++ roblox-news-guy.js
@@ -0,0 +1,4 @@
+javascript:(function(){
+  var audio = new Audio('https://github.com/Jamesy-tech/sound-bookmarklets/raw/main/roblox-news-guy.mp3');
+  audio.play().catch(function(e){console.log('Audio play failed:', e)});
+})();
